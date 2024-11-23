6c94daac375a7be5541f3a0abbfd64e21fcb04f6
aircross <aircross@users.noreply.github.com>
2024-11-23 20:52:03 +0800

    fix

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module cfddnsgo
+module cfddns
 
 go 1.22.3
 
--- package.sh
+++ package.sh
@@ -13,8 +13,6 @@ mkdir -p ./release/packages
 
 os_all='linux windows darwin freebsd android'
 arch_all='386 amd64 arm arm64 mips64 mips64le mips mipsle riscv64 loong64'
-# os_all='linux windows'
-# arch_all='386 amd64'
 extra_all='_ hf'
 
 cd ./release
